7b2d70c527a515a3d6a56824fac1ed10f5a736d7
Adrian Lackey <adrian.lackey2@mail.dcu.ie>
2021-01-28 12:25:19 +0000

    Set up Customer,Business Owner, Bookings, and Booking order models and conected to django admin

--- src/GymNow_site/pages/admin.py
+++ src/GymNow_site/pages/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+from .models import *
+
+
+admin.site.register(Customer)
+admin.site.register(Business_Owner)
+admin.site.register(Bookings)
+admin.site.register(Bookings_Order)
+
+
+
--- src/GymNow_site/pages/models.py
+++ src/GymNow_site/pages/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    customer_name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.customer_name
+
+class Business_Owner(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    business_name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(max_length=200, null=True)
+    phone = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.business_name
+
+class Bookings(models.Model):
+    CATEGORY = (
+        ('Gym Booking', 'Gym Booking'),
+        ('Personal Trainer', 'Personal Trainer'),
+        ('Pilates', 'Pilates'),
+        ('Yoga', 'Yoga'),
+        )
+
+    name = models.CharField(max_length=200, null=True)
+    business_name = models.ForeignKey(Business_Owner, null=True, on_delete = models.CASCADE)
+    price = models.FloatField(null=True)
+    description = models.CharField(max_length=200, null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Bookings_Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Paid', 'Paid'),
+        ('Confirmed', 'Confirmed'),
+        ('Cancelled', 'Cancelled'),
+        )
+    
+    customer = models.ForeignKey(Customer, null=True, on_delete= models.CASCADE)
+    business_name = models.ForeignKey(Business_Owner, null=True, on_delete = models.CASCADE)
+    booking = models.ForeignKey(Bookings, on_delete= models.CASCADE)
+    order_status = models.CharField(max_length=200, null=True, choices=STATUS)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+
+
