0ce34627ac721a884e9e0ae37d6bf36c30fc8d3a
Adrian Lackey <adrian.lackey2@mail.dcu.ie>
2021-01-29 11:20:37 +0000

    Introduced many gyms have many avaible bookings models relationship

--- src/GymNow_site/pages/models.py
+++ src/GymNow_site/pages/models.py
@@ -57,7 +57,6 @@ class Bookings(models.Model):
 
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    location = models.ForeignKey(Location, null=True, on_delete = models.CASCADE)
     description = models.CharField(max_length=200, null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
@@ -100,7 +99,7 @@ class Business_Owner(models.Model):
     email = models.EmailField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    available_bookings = models.ForeignKey(Bookings, null=True, on_delete=models.CASCADE)
+    available_bookings = models.ManyToManyField(Bookings)
 
     def __str__(self):
         return self.business_name
@@ -118,6 +117,9 @@ class Bookings_Order(models.Model):
     booking = models.ForeignKey(Bookings, on_delete= models.CASCADE)
     order_status = models.CharField(max_length=200, null=True, choices=STATUS)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.customer, self.business_name, self.booking, self.order_status
 
 
 
