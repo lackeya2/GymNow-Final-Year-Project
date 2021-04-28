448d1ecc30b7b7e3297c971e4d6b7f98d64e4c5f
Adrian Lackey <adrian.lackey2@mail.dcu.ie>
2021-04-28 22:45:21 +0100

    signup error fix

--- src/GymNow_site/pages/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from .models import Customer
-
-@receiver(post_save, sender=User)
-def Create_User_Profile(sender, instance, created, **kwargs):
-    if created:
-        Customer.objects.create(user=instance)
